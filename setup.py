ea882876547d06b79bb59a9e3f260fb65a44d0d3
Casper Beyer <caspervonb@octet.dev>
2024-09-10 15:15:00 +0200

    Bump version
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 # These are here for GitHub's dependency graph and help with setuptools support in some environments.
 setup(
     name="nkeys",
-    version='0.2.0',
+    version='0.2.1',
     license='Apache 2 License',
     packages=['nkeys'],
     install_requires=['pynacl'],
